509d675f420a1c9c7e59b7439b84ec92fa40593a
Gaurav Patkotwar <gauravpatkotwar29@gmail.com>
2026-08-04 00:30:07 +0530

    Clean next.config.js to resolve build error

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,14 +1,6 @@
-const path = require('path');
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export',
-  distDir: 'out',
-  outputFileTracingRoot: path.join(__dirname),
-  typescript: {
-    ignoreBuildErrors: true,
-  },
   images: {
     unoptimized: true,
   },
